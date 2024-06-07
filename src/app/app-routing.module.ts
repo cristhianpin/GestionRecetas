6b3c6b1514aa6d716e002c3123d81be19ee071ca
cristhianpin <cristhianpin92@gmail.com>
2024-06-06 21:57:13 -0500

    Init Repository to github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './PreLogueo/login/login.component';
+import { AcercaDeComponent } from './PreLogueo/acerca-de/acerca-de.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  { path: 'AcercaDe', component: AcercaDeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './PreLogueo/login/login.component';
+import { AcercaDeComponent } from './PreLogueo/acerca-de/acerca-de.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent,
+    AcercaDeComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
